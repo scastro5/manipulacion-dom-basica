c643101b0612a07e744ef5499bdb10e9c749ce5c
scastro5 <santiagoc505@gmail.com>
2022-12-14 18:19:41 -0500

    primeros archivos

---
+++ script.js
@@ -0,0 +1,17 @@
+const h1 = document.querySelector("h1");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btncalcular");
+const presult = document.querySelector("#result");
+btn.addEventListener("submit", sumarInput);
+const form = document.querySelector("#form");
+
+/* function sumarInput(event) {
+    //console.log({event});
+    event.preventDefault();
+    const sumainputs = input1.value + input2.value;
+    presult.innerHTML = "resultado: " + sumainputs;
+}
+*/
+
+
